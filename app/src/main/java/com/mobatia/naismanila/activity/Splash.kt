1f66e8c7c2e37aad72ef542eed198ecbdd102584
Sanju Sabu <sanju.sabu@mobatia.com>
2023-08-04 17:43:57 +0530

    Initial commit

---
+++ app/src/main/java/com/mobatia/naismanila/activity/Splash.kt
@@ -0,0 +1,12 @@
+package com.mobatia.naismanila.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mobatia.naismanila.R
+
+class Splash : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
